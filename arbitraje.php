0886695af05e0e6c32a792c552db8750953065d0
KarlxS <cj.cfaw@gmail.com>
2019-04-30 12:53:29 -0500

    sevicios  1.0

---
+++ arbitraje.php
@@ -0,0 +1,77 @@
+<?php require 'header/doctype.php';?>
+<link rel="stylesheet" href="public/css/arbitraje.css">
+<?php require 'header/title.php';
+      require 'header.php';?>
+<section>
+    <div class="box">
+        <div class="content-img">
+            <h2>ARBITRAJE</h2>
+
+            <figure>
+                <img src="public/img/arbitraje_Logo.jpg" alt="">
+            </figure>    
+        </div>
+        
+        <div class="content-Arbitraje">
+            <h2>¿QUIENES SOMOS?</h2>
+
+            <p><small>El Centro de Arbitraje CIP-CDLL, es un organismo interno del Consejo Departamental de la Libertad, conformado por profesionales en Ingeniería , con el objetivo de contribuir a resolver las controversias o discrepancias que puedan surgir en las actividades vinculadas a la Ingeniería en general, sin ninguna limitación.</small></p>
+
+            <h2>HISTORIA</h2>
+
+            <p><small>El estado peruano al emitir la Ley General de Arbitraje: N° 26572, publicado en El Peruano en Enero de 1996, surge la necesidad en el Departamento de la Libertad, evaluar, analizar, resolver las controversias entre dos personas naturales y/o jurídicas, mediante los métodos alternativos de la resolución de conflictos denominados MARC. El Centro de Arbitraje CA-CIP-CDLL, se constituyó el 19 de Junio del 2000, por acuerdo de la Junta Directiva del Consejo Departamental de la Libertad- Colegio de Ingenieros del Peru. En la actualidad el Reglamento y Estatuto de Centro, están basados en La Ley General de Arbitraje, N° 26572 y modificatorias, Nueva Ley de Arbitraje – Decreto Legislativo N° 1071.</small></p>
+
+            <h2>MISION</h2>
+
+            <p><small>Las controversias, discrepancias y conflictos que presenten las partes ( personas naturales y/o jurídicas), transformarlas en una cultura de paz, que coadyuven al desarrollo de la Ingeniería y del País.</small></p>
+
+            <h2>VISION</h2>
+
+            <p><small>Que el CA-CIP-CDLL, se consolide como una institución preventiva de las discrepancias, controversias y conflictos, amparados en la ética, experiencia, imparcialidad y neutralidad.</small></p>
+
+            <h2>CLAUSULA ARBITRAL</h2>
+
+            <p><small>Las partes acuerdan que todo litigio o controversia resultante de este contrato o relativo a este, se resolverá mediante arbitraje organizado y administrado por el Centro de Arbitraje del Colegio de Ingenieros del Peru – Consejo Departamental de la Libertad, de conformidad con sus reglamentos vigentes, a los cuales las partes se someten libremente, señalando que el laudo que se emita en el proceso será inapelable y definitivo.</small></p>
+        </div>
+
+        <div class="content-PDF">
+            <div class="card">
+                <div>
+                    <i class="fas fa-file-pdf icon"></i>
+                </div>
+                
+                <div class="cod-PDF">
+                    <p class="text-strong cod-margin"><small>Codigo de Ética.pdf</small></p>
+
+                    <a href="http://www.cip-trujillo.org/pdf/centro_peritaje/decreto-legislativo-n1071-2017 -2.pdf" download="decreto-legislativo-n1071-2017 -2.pdf">Descargar</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <i class="fas fa-file-pdf icon"></i>
+                </div>
+                
+                <div class="cod-PDF">
+                    <p class="text-strong cod-margin"><small>Codigo de Ética.pdf</small></p>
+
+                    <a href="http://www.cip-trujillo.org/pdf/centro_peritaje/decreto-legislativo-n1071-2017 -2.pdf" download="decreto-legislativo-n1071-2017 -2.pdf">Descargar</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <div>
+                    <i class="fas fa-file-pdf icon"></i>
+                </div>
+                
+                <div class="cod-PDF">
+                    <p class="text-strong cod-margin"><small>Codigo de Ética.pdf</small></p>
+
+                    <a href="http://www.cip-trujillo.org/pdf/centro_peritaje/decreto-legislativo-n1071-2017 -2.pdf" download="decreto-legislativo-n1071-2017 -2.pdf">Descargar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php require 'footer.php';?>
+<script type="text/javascript" src="public/js/nav.js"></script>
---
+++ arbitros_ingenieros.php
@@ -0,0 +1,31 @@
+<?php require 'header/doctype.php';?>
+<link rel="stylesheet" href="public/css/arbitros_ingenieros.css">
+<?php require 'header/title.php';
+      require 'header.php';?>
+<section>
+    <div class="box">
+        <h2>ARBITROS INGENIEROS</h2>
+
+        <table>
+            <thead>
+                <tr>
+                    <th class="table-name" colspan="2">NOMINA DE ARBITROS INGENIEROS (PERIODO 2016)</th>
+                </tr>
+
+                <tr>
+                    <th class="table-contend">NOMBRE</th>
+                    <th class="table-contend">ESPECIALIDAD</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td class="name-text">JORGE CARLOS SIFUENTES AYALA</td>
+                    <td>Contrataciones con el Estado, Construcción, Mecánico</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</section>
+<?php require 'footer.php';?>
+<script type="text/javascript" src="public/js/nav.js"></script>
