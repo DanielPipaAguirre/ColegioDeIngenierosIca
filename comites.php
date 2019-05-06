0824bea66b1ef151a294f3c6ea9c0204bf3d079a
KarlxS <cj.cfaw@gmail.com>
2019-05-06 11:48:26 -0500

    .

--- comites.php
+++ comites.php
@@ -4,150 +4,113 @@
       require 'header.php';?>
 
 <body>
-      
-   
+    <table class="listado12">
+        <thead>
+            <tr>
+                <th colspan="2"><h1>Comite Local De Pisco</h1></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>PRESIDENTE</td>
+                <td>ING.Ponce Manzuelos Armando Ricardo</td>
+            </tr>
+            <tr>
+                <td>VICE-PRECIDENTE</td>
+                <td>ING.Rosas Leon Damas Roberto</td>                	
+            </tr>
+            <tr>
+                <td>SECRETARIO</td>
+                <td>ING.Papen Bernaola Ricardo Javier</td>                		
+            </tr>
+            <tr>
+                <td>TESORERO</td>
+                <td>ING.Elias Yupanqui Victor Hernan</td>                		
+            </tr>
+            <tr>
+                <td>VOCAL</td>
+                <td>ING.Palomino Gallegos Percy Antonio</td>                		
+            </tr>
+            <tr>
+                <td>VOCAL</td>
+                <td>ING.Hernandez Castillo Antonio Floriestan</td>    
+            </tr> 
+        </tbody>
+    </table>
+    <table class="listado12" id="listado12">
+        <thead>
+            <tr>
+                <th colspan="2"><h1>Comite Local De Chincha</h1></th>               
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>PRESIDENTE</td>
+                <td>ING.Ponce Manzuelos Armando Ricardo</td>                   
+            </tr>
+            <tr>
+                <td>VICE-PRECIDENTE</td>
+                <td>ING.Rosas Leon Damas Roberto</td>              	
+            </tr>
+            <tr>
+                <td>SECRETARIO</td>
+                <td>ING.Papen Bernaola Ricardo Javier</td>                		
+            </tr>
+            <tr>
+                <td>TESORERO</td>
+                <td>ING.Elias Yupanqui Victor Hernan</td>                		
+            </tr>
+            <tr>
+                <td>VOCAL</td>
+                <td>ING.Palomino Gallegos Percy Antonio</td>                		
+            </tr>
+            <tr>
+                <td>VOCAL</td>
+                <td>ING.Hernandez Castillo Antonio Floriestan</td>    
+            </tr>
+        </tbody>
+    </table>
+    <table class="listado12" id="listado12">
+        <thead>
+            <tr>
+                <th colspan="2"><h1>Comite Local De Nazca</h1></th>               
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                    <td>PRESIDENTE</td>
+                    <td>ING.Ponce Manzuelos Armando Ricardo</td>                   
+            </tr>
+            <tr>
+                <td>VICE-PRECIDENTE</td>
+                <td>ING.Rosas Leon Damas Roberto</td>
+                	
+            </tr>
+            <tr>
+                <td>SECRETARIO</td>
+                <td>ING.Papen Bernaola Ricardo Javier</td>
+                		
+            </tr>
+            <tr>
+                <td>TESORERO</td>
+                <td>ING.Elias Yupanqui Victor Hernan</td>
+                		
+            </tr>
+            <tr>
+                <td>VOCAL</td>
+                <td>ING.Palomino Gallegos Percy Antonio</td>
+                		
+            </tr>
+            <tr>
+                    <td>VOCAL</td>
+                    <td>ING.Hernandez Castillo Antonio Floriestan</td>
     
-            <table class="listado12">
-                <thead>
-                    <tr>
-                        <th colspan="2"><h1>Comite Local De Pisco</h1></th>
-                       
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                            <td>PRESIDENTE</td>
-                            <td>ING.Ponce Manzuelos Armando Ricardo</td>
-                           
-                    </tr>
-                    <tr>
-                        <td>VICE-PRECIDENTE</td>
-                        <td>ING.Rosas Leon Damas Roberto</td>
-   
-                        	
-                    </tr>
-                    <tr>
-                        <td>SECRETARIO</td>
-                        <td>ING.Papen Bernaola Ricardo Javier</td>
-   
-                        		
-                    </tr>
-                    <tr>
-                        <td>TESORERO</td>
-                        <td>ING.Elias Yupanqui Victor Hernan</td>
-   
-                        		
-                    </tr>
-                    <tr>
-                        <td>VOCAL</td>
-                        <td>ING.Palomino Gallegos Percy Antonio</td>
-    
-                        		
-                    </tr>
-                    <tr>
-                            <td>VOCAL</td>
-                            <td>ING.Hernandez Castillo Antonio Floriestan</td>
-        	
-                    </tr>
-                  
-                
-                </tbody>
-            </table>
-
-            <table class="listado12" id="listado12">
-                <thead>
-                    <tr>
-                        <th colspan="2"><h1>Comite Local De Chincha</h1></th>
-                       
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                            <td>PRESIDENTE</td>
-                            <td>ING.Ponce Manzuelos Armando Ricardo</td>
-                           
-                    </tr>
-                    <tr>
-                        <td>VICE-PRECIDENTE</td>
-                        <td>ING.Rosas Leon Damas Roberto</td>
-   
-                        	
-                    </tr>
-                    <tr>
-                        <td>SECRETARIO</td>
-                        <td>ING.Papen Bernaola Ricardo Javier</td>
-   
-                        		
-                    </tr>
-                    <tr>
-                        <td>TESORERO</td>
-                        <td>ING.Elias Yupanqui Victor Hernan</td>
-   
-                        		
-                    </tr>
-                    <tr>
-                        <td>VOCAL</td>
-                        <td>ING.Palomino Gallegos Percy Antonio</td>
-    
-                        		
-                    </tr>
-                    <tr>
-                            <td>VOCAL</td>
-                            <td>ING.Hernandez Castillo Antonio Floriestan</td>
-        	
-                    </tr>
-                  
-                
-                </tbody>
-            </table>
-
-            <table class="listado12" id="listado12">
-                <thead>
-                    <tr>
-                        <th colspan="2"><h1>Comite Local De Nazca</h1></th>
-                       
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                            <td>PRESIDENTE</td>
-                            <td>ING.Ponce Manzuelos Armando Ricardo</td>
-                           
-                    </tr>
-                    <tr>
-                        <td>VICE-PRECIDENTE</td>
-                        <td>ING.Rosas Leon Damas Roberto</td>
-   
-                        	
-                    </tr>
-                    <tr>
-                        <td>SECRETARIO</td>
-                        <td>ING.Papen Bernaola Ricardo Javier</td>
-   
-                        		
-                    </tr>
-                    <tr>
-                        <td>TESORERO</td>
-                        <td>ING.Elias Yupanqui Victor Hernan</td>
-   
-                        		
-                    </tr>
-                    <tr>
-                        <td>VOCAL</td>
-                        <td>ING.Palomino Gallegos Percy Antonio</td>
-    
-                        		
-                    </tr>
-                    <tr>
-                            <td>VOCAL</td>
-                            <td>ING.Hernandez Castillo Antonio Floriestan</td>
-        	
-                    </tr>
-                  
-                
-                </tbody>
-            </table>
-            <?php require 'footer.php';?>
+            </tr>
+          
+        
+        </tbody>
+    </table>
+<?php require 'footer.php';?>
 <script src="public/js/nav.js"></script>
 </body>
 </html>
