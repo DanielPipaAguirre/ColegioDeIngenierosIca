7359b0c823089bf10c29aa24b9699a474dd75c1e
Arnold <proustxd@users.noreply.github.com>
2019-05-07 10:18:10 -0500

    comisiones

---
+++ comisiones.php
@@ -0,0 +1,457 @@
+<?php require 'header/doctype.php';?>
+    <link rel="stylesheet" href="public/css/comisiones.css">
+    <?php require 'header/title.php';
+      require 'header.php';?>
+
+<body>
+
+<!-- <section class="subcaja">
+        <nav class="menu1">
+            <input type="checkbox" id="togglemenu" checked />
+            <label for="togglemenu" class="togglemenu"><h3 class="cat">Categorias</h3></label>
+            <ul>    
+                <li class="todo" id="all"><a>Presentación del Cip</a></li>
+                <li class="so-p1" id="conference-room"><a>Salón de Conferencias</a></li>
+                <li class="so-p2" id="audience"><a>Auditorio</a></li>
+                <li class="so-p3" id="party-room"><a>Salón de Fiestas</a></li>
+                <li class="so-p3" id="local-countryside"><a>Local Campestre</a></li>
+                <li class="so-p3" id="sports-slab"><a>Losa Deportiva</a></li>
+                <li class="so-p6" id="swimming-pool"><a>Piscina</a></li>
+                <li class="so-p7" id="swimming-pool"><a>Piscina</a></li>
+                <li class="so-p8" id="swimming-pool"><a>Piscina</a></li>
+                <li class="so-p9" id="swimming-pool"><a>Piscina</a></li>
+                <li class="so-p10" id="swimming-pool"><a>Piscina</a></li>
+                <li class="so-p11" id="swimming-pool"><a>Piscina</a></li>
+            </ul>
+        </nav>
+    </section>
+<section class="subcaja2"> -->
+    <table class="listado12 p1">
+        <thead>
+            <tr>
+                <th colspan="2"><h1>Comisión Departamental de Colegiatura</h1></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>PRESIDENTE</td>
+                <td>ING.Dante Vladimir Campos Valencia</td>
+            </tr>
+            <tr>
+                <td>VOCAL-PERMANENTE 1</td>
+                <td>ING.Dante Fermin Calderon Huamani</td>                	
+            </tr>
+            <tr>
+                <td>VOCAL-PERMANENTE 2</td>
+                <td>ING.Alejandro Sulpicio Escate Hernandez</td>                		
+            </tr>
+            <tr>
+                <td>VOCAL-SUPLENTE</td>
+                <td>ING.Ricardo Felix Tello Medina</td>                		
+            </tr>
+           
+        </tbody>
+    </table>
+    <table class="listado12 p2" id="listado12">
+        <thead>
+            <tr>
+                <th colspan="2"><h1>Comisión Departamental de Ética</h1></th>               
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>PRESIDENTE</td>
+                <td>ING.Martin Hamiltos Eilson Huamanchuco</td>                   
+            </tr>
+            <tr>
+                <td>MIEMBRO TITULAR</td>
+                <td>ING.Jose Alejandro Herrera Hernandez</td>              	
+            </tr>
+            <tr>
+                <td>MIEMBRO TITULAR</td>
+                <td>ING.Yelfri Mario Huachua Flores</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO SUPLENTE</td>
+                <td>ING.Luis Alberto Ramirez Arroyo</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO SUPLENTE</td>
+                <td>ING.Hermilla Quispe Velasaquez</td>                		
+            </tr>
+           
+        </tbody>
+    </table>
+    <table class="listado12 p3" id="listado12">
+        <thead>
+            <tr>
+                <th colspan="2"><h1>Comisión IEPI</h1></th>               
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                    <td>PRESIDENTE</td>
+                    <td>ING.Felipe Estuardo Yarasca Arcos</td>                   
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Luis Enrique Mina Aparicio</td>
+                	
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Jenny Ryna Araujo Huaraca</td>
+                		
+            </tr>
+          
+        </tbody>
+    </table>
+        <table class="listado12 p4" id="listado12">
+        <thead>
+            <tr>
+                <th colspan="2"><h1>Comisión Instituto del Bienestar del ING</h1></th>               
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>PRESIDENTE</td>
+                <td>ING.Jorge Luis Hernandez Angulo</td>                   
+            </tr>
+            
+           
+        </tbody>
+    </table>
+    <table class="listado12 p5" id="listado12">
+        <thead>
+            <tr>
+                <th colspan="2"><h1>Comisión Departamental del ISS</h1></th>               
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                    <td>PRESIDENTE</td>
+                    <td>ING.Reymundo Calderon Pino</td>                   
+            </tr>
+            
+        </tbody>
+    </table>
+    <table class="listado12 p6" id="listado12">
+        <thead>
+            <tr>
+                <th colspan="2"><h1>Comisión de Recursos Hdricos</h1></th>               
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>PRESIDENTE</td>
+                <td>ING.Pedro Luis Orellana Parvina</td>                   
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Armando Ricardo Ponce Manzuelos</td>              	
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Jóse Melchor Ghezzi Hernandez</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Herbert Willian Suiney Pacheco</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Luis Adolfo Miranda Gutiérrez</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Evelina Alida Yarasca Rangel</td>              	
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Tomas Edinson Cabello Carrillo</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Hilda Rosa Luna Zapata</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Jóse Graciano Muñante Valenzuela</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Antonio Floriestan Hernandez Castillo</td>                		
+            </tr>
+           
+        </tbody>
+    </table>
+    <table class="listado12 p7" id="listado12">
+        <thead>
+            <tr>
+                <th colspan="2"><h1>Comisión Departamental de Defensa Profesional</h1></th>               
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                    <td>PRESIDENTE</td>
+                    <td>ING.Reymundo Calderon Pino</td>                   
+            </tr>
+            <tr>
+                <td>INTEGRANTE</td>
+                <td>ING.Jimmy Felipe Muñoz Guzmán</td>
+                	
+            </tr>
+            <tr>
+                <td>INTEGRANTE</td>
+                <td>ING.César Martin Paul Bedriñana Rojas</td>
+                		
+            </tr>
+            <tr>
+                <td>INTEGRANTE</td>
+                <td>ING.Jorge Luis De La Cruz Martinez</td>
+                		
+            </tr>
+            <tr>
+                <td>INTEGRANTE</td>
+                <td>ING.Roberto Fernando Flores Valenzuela</td>
+                		
+            </tr>
+          
+        </tbody>
+    </table>
+
+    <table class="listado12 p8" id="listado12">
+        <thead>
+            <tr>
+                <th colspan="2"><h1>Comisión de Asuntos Ambientales</h1></th>               
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>PRESIDENTE</td>
+                <td>ING.Carlos Blanco Contreras</td>                   
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Luis Alberto Robles Arana</td>              	
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Juan Valdivia Rosales</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Elder Daniel Agüero Rospigliosi</td>
+            </tr>
+                                		
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Santos Humberto Olivera Manchado</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Juan Cristhian Soto Huancahuari</td>              	
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Oliver Rafael Mendoza Navarrete</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Carlos Aníbal Bendezú  </td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Manuel Augusto Ormeño Morales</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Lourdes Amella De La Cru< Huaracc</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Milagros Valdivia Pérez</td>                		
+            </tr>
+           
+        </tbody>
+    </table>
+    <table class="listado12 p9" id="listado12">
+        <thead>
+            <tr>
+                <th colspan="2"><h1>Comisión de Vigilancia de la Obra Pública</h1></th>               
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                    <td>PRESIDENTE</td>
+                    <td>ING.Edson Rios Villagomez</td>                   
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Dante Vladimir Campos Valencia</td>
+                	
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Néstor Arturo Cubas Tispana</td>
+                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Ricardo Felix Tello Medina</td>
+                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Luis Enrique Mina Aparicio</td>
+                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Jenny Reyna Araujo Huaraca</td>
+                	
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Alejandro Sulpicio Escate Hernandez</td>
+                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Iván Alexander Morales Cahuana</td>
+                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Dante Fermin Calderon Huamani</td>
+                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Roberto Fernando Flores Valenzuela</td>
+                		
+            </tr>
+          
+        </tbody>
+    </table>
+    <table class="listado12 p10" id="listado12">
+        <thead>
+            <tr>
+                <th colspan="2"><h1>Comisión de Gestión de Riesgos de Desastres</h1></th>               
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>PRESIDENTE</td>
+                <td>ING.Oscar Manuel Ore Herrera</td>                   
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Ysaias César Olaechea Gameros</td>              	
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Luis Alberto Cancho Ojeda</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Luis Alberto Ordoñez Fuentes</td> 
+            </tr>
+                               		
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Gerardo Arevalo Arevalo</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Juan Alfredo Manchego Meza</td>              	
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Uberto Alcides Ochante Fajardo</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Juan Christian Meza Morales  </td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Faustino Eusebio Moreno Montiel</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Katherine Lizeth Reyes Gutiérrez</td>                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Edin Rony Reyes Checclo</td>                		
+            </tr>
+           
+        </tbody>
+    </table>
+    <table class="listado12 p11" id="listado13">
+        <thead>
+            <tr>
+                <th colspan="2"><h1 id="tamaño">Comisión de Acondicionamiento Territorial y Desarrollo Urbano Sostenible</h1></th>               
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                    <td>PRESIDENTE</td>
+                    <td>ING.Javier Hermogenes Cornejo Ventura</td>                   
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Victor Manuel Buleje Calle</td>
+                	
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Juan Luis Palomino Jurado</td>
+                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Iván Alexander Morales Cahuana</td>
+                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Damaso Roberto Rosas León</td>
+                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Efráin Manuel Achamiso Meneses</td>
+                	
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Ciro Pedro Ayala Pacheco</td>
+                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Ysela Karina Campos Aybar</td>
+                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Dino Jesús Mora Castillo</td>
+                		
+            </tr>
+            <tr>
+                <td>MIEMBRO</td>
+                <td>ING.Herbert Willian Suiney Pacheco</td>
+                		
+            </tr>
+          
+        </tbody>
+    </table>
+<!-- </section> -->
+<!-- <script src="public/js/jquery.js"></script>
+<script src="public/js/comisiones.js"></script> -->
+<?php require 'footer.php';?>
+<script src="public/js/nav.js"></script>
+</body>
+</html>
